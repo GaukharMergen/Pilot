43f3bedc3db7948fbd2edd7b8368b1ea1560ed27
Gaukhar <gauvik2@gmail.com>
2014-09-04 14:44:27 +0000

    isparavlenie oshibki

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
    before_action :correct_user,   only: :destroy
+  
  def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -15,13 +16,12 @@ class MicropostsController < ApplicationController
     redirect_to root_url
   end
     
-  end
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
     end
-def correct_user
+   def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
     end
